10c8dcef7838f32d50e6589ede79c106d180aa04
Acid Opal <acidnaufal@gmail.com>
2016-09-01 07:42:49 +0700

    Update UserController.php

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -6,11 +6,11 @@ class UserController extends \Phalcon\Mvc\Controller
 
     public function indexAction()
     {
-      if ($this->session->has('auth')){
+       if ($this->session->has('auth')){
 
-       }else{
-        $this->response->redirect('login');
-      }
+        }else{
+          $this->response->redirect('login');
+        }
     	$data_user = User::find();
     	$this->view->data_user=$data_user;
     }
